d2148a298a5b0caa03c69ede46e141ad3026c894
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-05-11 02:29:48 +0600

    Solved 3 task

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,28 @@
 "use strict";
-console.log("hello world");
+function formatString(input, toUpper) {
+    if (toUpper || typeof toUpper === "undefined") {
+        return input.toUpperCase();
+    }
+    else {
+        return input.toLowerCase();
+    }
+}
+function filterByRating(items) {
+    const highestRatings = items.filter((item) => item.rating >= 4);
+    return highestRatings;
+}
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 3.9 }
+// ];
+// console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+function concatenateArrays(...arrays) {
+    let array = [];
+    arrays.forEach((singleArray) => singleArray.forEach((value) => array.push(value)));
+    return array;
+}
+const b = concatenateArrays(["a", "b"], ["c"]); // Output: ["a", "b", "c"]
+const a = concatenateArrays([1, 2], [3, 4], [5]); // Output: [1, 2, 3, 4, 5]
+console.table({ a, b });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,45 @@
-console.log("hello world");
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper || typeof toUpper === "undefined") {
+    return input.toUpperCase();
+  } else {
+    return input.toLowerCase();
+  }
+}
+
+// console.log(formatString("Hello"),
+// formatString("Hello", true)  ,
+// formatString("Hello", false)
+// );
+
+type Items = {
+  title: string;
+  rating: number;
+};
+
+type ItemsOfArray = Items[];
+
+function filterByRating(items: ItemsOfArray): ItemsOfArray {
+  const highestRatings: ItemsOfArray = items.filter(
+    (item: Items) => item.rating >= 4
+  );
+  return highestRatings;
+}
+
+// const books = [
+//   { title: "Book A", rating: 4.5 },
+//   { title: "Book B", rating: 3.2 },
+//   { title: "Book C", rating: 3.9 }
+// ];
+
+// console.log(filterByRating(books));
+// Output: [ { title: "Book A", rating: 4.5 }, { title: "Book C", rating: 5.0 } ]
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  let array: T[] = [];
+  arrays.forEach((singleArray) => singleArray.forEach((value) => array.push(value)));
+  return array;
+}
+
+const b= concatenateArrays(["a", "b"], ["c"]); // Output: ["a", "b", "c"]
+const a =concatenateArrays([1, 2], [3, 4], [5]); // Output: [1, 2, 3, 4, 5]
+console.table({a,b})
